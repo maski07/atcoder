222b1fb6577d762a038c76220146ba38097ab8dc
maski07 <energym795213@gmail.com>
2020-06-14 13:02:54 +0900

    自動テストの別のやり方を追加

---
+++ run2.sh
@@ -0,0 +1,17 @@
+filename=200.py
+python3 $filename << EOS
+a b
+EOS
+python3 $filename << EOS
+a b
+EOS
+python3 $filename << EOS
+a b
+EOS
+
+# 補足
+# sh実行時のargsを読み取って実行する方法もある！！
+# for arg in "$@"; do
+#     echo "file:". ${arg}
+#     python3 200.py < ${arg}
+# done
