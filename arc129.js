d22e35cfb13b6785b2434bed79d63e56b580147e
maski07 <energym795213@gmail.com>
2021-11-27 22:33:10 +0900

    arc129.js

---
+++ arc129.js
@@ -0,0 +1,10 @@
+/** メモ **/
+'use strict'
+/**  */
+var log = console.log;
+var splitN = (N) => N.split().split(' ').map(Number);
+function MainA(input){
+	
+}
+// MainA(require("fs").readFileSync("/dev/stdin", "utf8").trim());
+
