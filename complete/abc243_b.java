f9d461a11429de8b9ad17ae5749cf02625f3c3e2
maski07 <energym795213@gmail.com>
2022-04-15 16:28:02 +0900

    toolの拡充

--- complete/abc243_b.java
+++ complete/abc243_b.java
@@ -45,11 +45,28 @@ class Main {
         System.out.println(object);
     }
 
-    private static void logArray(Object[] obj) {
+    private static void log(Object... object) {
+        System.out.println(Arrays.toString(object));
+    }
+
+    private static void logDeepArray(Object[] obj) {
         System.out.println(Arrays.deepToString(obj));
     }
 
-    private static int toInt(String str) {
-        return Integer.parseInt(str);
+    public static class Util {
+        public static int[] getArray(int N, Scanner scanner) {
+            Function<Integer, int[]> get = (argN) -> {
+                var arr = new int[argN.intValue()];
+                for (var i = 0; i < argN.intValue(); i++) {
+                    arr[i] = scanner.nextInt();
+                }
+                return arr;
+            };
+            return get.apply(N);
+        }
+
+        private static int toInt(String str) {
+            return Integer.parseInt(str);
+        }
     }
 }
