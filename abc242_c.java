d352e7c20923fa68693f748881a5e8c4f6c7193a
maski07 <energym795213@gmail.com>
2022-04-17 17:55:30 +0900

    abc242_C

--- abc242_c.java
+++ abc242_c.java
@@ -1,3 +1,4 @@
+
 /** 既存メソッドで使用 */
 import java.util.Scanner;
 import java.util.function.Function;
@@ -14,18 +15,51 @@ import java.util.Comparator;
 import java.util.StringJoiner;
 import java.util.Comparator;
 
-
 class Main {
 
+    private static final int mod = 998244353;
+
+    private static int[][] memo;
+
     public static void main(String[] args) {
-        try(var scanner = new Scanner(System.in)){
-            var aa = scanner.nextInt();
-            solve();
+        try (var scanner = new Scanner(System.in)) {
+            var N = scanner.nextInt();
+            memo = new int[N][10];
+            for (var i = 1; i <= 9; i++) {
+                memo[0][i] = 1;
+            }
+            for (var i = 1; i < N; i++) {
+                for (var j = 1; j <= 9; j++) {
+                    for (var k = Math.max(1, j - 1); k <= Math.min(9, j + 1); k++) {
+                        memo[i][j] += memo[i - 1][k];
+                        memo[i][j] %= mod;
+                    }
+                }
+            }
+            var answer = 0;
+            for (var i = 1; i <= 9; i++) {
+                answer += memo[N - 1][i];
+                answer %= mod;
+            }
+            log(answer);
         }
     }
-    private static void solve() {
-        log("Yes");
-        log("No");
+
+    public static int dfs(int N, int now) {
+        if (now > 9 || now <= 0) {
+            return 0;
+        }
+        if (N == 0) {
+            return 1;
+        }
+        if (memo[N][now] != -1) {
+            return memo[N][now];
+        }
+        var count = dfs(N - 1, now) % mod
+                + dfs(N - 1, now - 1) % mod
+                + dfs(N - 1, now + 1) % mod;
+        memo[N][now] = count;
+        return count;
     }
 
     private static void log(Object object) {
@@ -38,6 +72,16 @@ class Main {
 
     private static void logDeepArray(Object[] obj) {
         System.out.println(Arrays.deepToString(obj));
+    }
+
+    private static int loggedCount = 0;
+
+    private static void timelog(Object... obj) {
+        final int logCount = 10;
+        if (loggedCount <= logCount) {
+            System.out.println(Arrays.toString(obj));
+            loggedCount++;
+        }
     }
 
     public static class Util {
