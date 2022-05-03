d4d6ea4991ee98c1c636bd3450578031f4dc82ed
maski07 <energym795213@gmail.com>
2022-05-03 17:47:23 +0900

    動的計画法_典型問題

--- dp_c.java
+++ dp_c.java
@@ -1,27 +1,39 @@
+
 /** 既存メソッドで使用 */
 import java.util.Scanner;
 import java.util.function.Function;
 import java.util.Arrays;
 /** よく使うやつを定義 */
 import java.lang.Math;
-import java.util.Comparator;
-import java.util.StringJoiner;
-/** サンプルコード */
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.OptionalInt;
 
 class Main {
 
     public static void main(String[] args) {
-        try(var scanner = new Scanner(System.in)){
-            var a = scanner.nextInt();
-            solve();
+        try (var scanner = new Scanner(System.in)) {
+            var N = scanner.nextInt();
+            var abcn = new int[N][3];
+            for (var i = 0; i < N; i++) {
+                abcn[i] = Util.getIntArray(3, scanner);
+            }
+            var dp = new int[3];
+            dp[0] = abcn[0][0];
+            dp[1] = abcn[0][1];
+            dp[2] = abcn[0][2];
+            for (var i = 1; i < N; i++) {
+                var a = dp[0];
+                var b = dp[1];
+                var c = dp[2];
+                dp[0] = Math.max(b, c) + abcn[i][0];
+                dp[1] = Math.max(c, a) + abcn[i][1];
+                dp[2] = Math.max(a, b) + abcn[i][2];
+                // log(Arrays.toString(dp));
+            }
+            log(Arrays.stream(dp).max().getAsInt());
         }
-    }
-    private static void solve() {
-        log("Yes");
-        log("No");
     }
 
     private static void log(Object object) {
@@ -61,23 +73,5 @@ class Main {
         private static int toInt(String str) {
             return Integer.parseInt(str);
         }
-    }
-}
-
-/**
- * よく使うコードリスト
- */
-public class SampleCode {
-    private static void Sample() {
-        /**
-         * ArrayList
-         * https://docs.oracle.com/javase/jp/8/docs/api/java/util/ArrayList.html
-         */
-        List<Integer> arrayList = new ArrayList<Integer>();
-        arrayList.indexOf(0); // indexを返却
-        arrayList.sort(Comparator.naturalOrder()); // ソート
-        /**
-         * 配列 https://docs.oracle.com/javase/jp/8/docs/api/java/sql/Array.html
-         */
     }
 }
