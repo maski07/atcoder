f7129afd38f1a1a5b135f65de1b3a781eeba68c4
maski07 <energym795213@gmail.com>
2022-08-07 10:42:53 +0900

    数学とmodの問題

--- abc258_c2.java
+++ abc258_c2.java
@@ -15,34 +15,30 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.HashMap;
 
+/** 解き直す */
 class Main {
 
     public static void main(String[] args) {
         try (var scanner = new Scanner(System.in)) {
             var N = scanner.nextInt();
             var Q = scanner.nextInt();
-            List<Character> S = scanner
-                    .next()
-                    .chars()
-                    .mapToObj(c -> (char) c)
-                    .collect(Collectors.toList());
-            List<Character> newS = new ArrayList<>(S);
-            final int size = newS.size();
+            var S = scanner.next().toCharArray();
+            var curr = 0;
+            var out = new StringBuilder();
             for (var i = 0; i < Q; i++) {
                 var t = scanner.nextInt();
                 var x = scanner.nextInt();
                 // log("i:", i, ",t:", t, ",x:", x);
                 if (t == 1) {
-                    var tmp = newS.subList(size - x, size);
-                    Collections.reverse(tmp);
-                    var tmp2 = newS.subList(0, size - x);
-                    tmp2.addAll(0, tmp);
-                    newS = tmp2;
+                    // 頭の文字が何文字目の文字なのかをもつ
+                    curr = (curr - x + N) % N;
                 } else if (t == 2) {
-                    log(newS);
-                    log(newS.get(x - 1));
+                    var index = (curr + x - 1) % N;
+                    out.append(S[index]);
+                    out.append("\n");
                 }
             }
+            log(out.toString());
         }
     }
 
